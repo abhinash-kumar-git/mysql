e6fbe3c644b0e8c5da29dc6d7ce11316cd7daa8a
kumar <kumar@DESKTOP-U8TU23O>
2023-04-23 15:38:37 +0530

    list

---
+++ DSAFILEMatch/src/DATASTRUCTUREFILE/COM/LinkedFile.java
@@ -0,0 +1,58 @@
+package DATASTRUCTUREFILE.COM;
+
+
+
+import java.util.ArrayList;
+
+import com.github.javafaker.Country;
+import com.github.javafaker.Faker
+;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.LinkedListMultimap;
+class Pr{
+	String n;
+	
+}
+
+public class LinkedFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Faker faker=new Faker();
+		//faker.address();
+	
+	//Country k=f.country();
+		//f.currency();
+		 //String h=k.currencyCode();
+		/*
+		 * System.out.println(); String streetName = faker.address().streetName();
+		 * String number = faker.address().buildingNumber(); String city =
+		 * faker.address().city(); String country = faker.address().country();
+		 * 
+		 * System.out.println(String.format("%s\n%s\n%s\n%s", number, streetName, city,
+		 * country));
+		 */
+		/*
+		 * LinkedList<Integer>list=new LinkedList<Integer>(); LinkedList<Comparable>
+		 * list1=new LinkedList<Comparable>();
+		 * 
+		 * list1.add("asd"); list1.add(12); list1.add(17899);
+		 * System.out.println(list1.get(0)); list.add(12); list.add(3); list.add(4);
+		 * list.add(5); list.add(6); list.addFirst(123); boolean f=list.offerLast(76);
+		 * list.removeIf(n->(n%2==1)) ; for(int v:list) { //System.out.println(v); }
+		 * ArrayList<Integer>ar=new ArrayList<Integer>(); ArrayList<Comparable> l = new
+		 * ArrayList<Comparable>(); l.add("ab"); l.add(89); l.add("wel");
+		 * System.out.println(l); ar.add(126); ar.add(124); ar.add(125); ar.add(127);
+		 * ar.add(128); ar.add(126); ar.add(124); ar.add(125); ar.add(127); ar.add(128);
+		 * System.out.println(ar.get(4)); ar.removeIf(n->(n%2==0)); for(int k:ar) {
+		 * //System.out.println(k); }
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+	}
+
+}
